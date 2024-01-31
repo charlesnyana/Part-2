1fd7d6b07980d408a8e1f531e868fa53809a0b59
nyanasen <nyanasen@OA-S141-05>
2024-01-31 10:59:56 -0500

    Task 2 complete: plane follows path and lands.

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -1,21 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class Plane : MonoBehaviour
 {
     public List<Vector2> points;
     public float newPointThreshold = 0.2f;
+    public float speed = 1f;
     Vector2 lastPosition;
     LineRenderer lineRenderer;
+    Vector2 currentPosition;
+    Rigidbody2D rb;
+    public AnimationCurve landing;
+    float landingTimer;
 
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        rb = GetComponent<Rigidbody2D>();
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0, transform.position);
     }
 
+    private void FixedUpdate()
+    {
+       currentPosition = new Vector2(transform.position.x, transform.position.y); 
+        if (points.Count > 0)
+        {
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            rb.rotation = -angle;
+        }
+        rb.MovePosition(rb.position + (Vector2)transform.up * speed * Time.deltaTime);
+    }
+
+    private void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            landingTimer += 0.1f * Time.deltaTime;
+            float interpolation = landing.Evaluate(landingTimer);
+            if (transform.localScale.z < 0.1f)
+            {
+                Destroy(gameObject);
+            } else
+            {
+                transform.localScale = Vector3.Lerp(transform.localScale, Vector3.zero, interpolation);
+            }
+        }
+
+        if(points.Count > 0)
+        {
+            if(Vector2.Distance(currentPosition, points[0]) < newPointThreshold)
+            {
+                points.RemoveAt(0);
+
+                for(int i = 0; i < lineRenderer.positionCount - 2; i++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i + 1));
+                }
+                lineRenderer.positionCount--;
+            }
+        }
+    }
 
     private void OnMouseDown()
     {
