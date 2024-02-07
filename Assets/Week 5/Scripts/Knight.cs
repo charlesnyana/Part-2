b6e52a553c5faba714efa541e3b0dd5456ad424e
nyanasen <nyanasen@OA-S141-06>
2024-02-07 11:05:24 -0500

    Task 1 complete: animations and basic damage/death logic implemented.

---
+++ Assets/Week 5/Scripts/Knight.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knight : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    Vector2 destination;
+    Vector2 movement;
+    public float speed = 3f;
+    Animator animator;
+    bool clickOnSelf = false;
+    public float HP;
+    public float maxHP = 5;
+    bool isDead;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        HP = maxHP;
+        isDead = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isDead) return;
+
+        movement = destination - (Vector2)transform.position;
+
+        if (movement.magnitude < 0.1)
+        {
+            movement = Vector2.zero;
+        }
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
+    }
+
+    void Update()
+    {
+        if (isDead) return;
+
+        if (Input.GetMouseButtonDown(0) && !clickOnSelf)
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        animator.SetFloat("Movement", movement.magnitude);
+    }
+
+    private void OnMouseDown()
+    {
+        if (isDead) return;
+
+        clickOnSelf = true;
+        takeDamage(1);
+    }
+    private void OnMouseUp()
+    {
+        clickOnSelf = false;
+    }
+    private void takeDamage(float damage)
+    {
+        HP -= damage;
+
+        HP = Mathf.Clamp(HP, 0, maxHP);
+
+        if (HP <= 0)
+        {
+            isDead = true;
+            animator.SetTrigger("Death");
+        } else
+        {
+            isDead = false;
+            animator.SetTrigger("TakeDamage");
+        }
+    }
+}
