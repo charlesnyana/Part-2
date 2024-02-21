bdc00253c63efea8a85e73edb2f265a8d67c5743
nyanasen <nyanasen@OA-S141-04>
2024-02-21 11:26:41 -0500

    task 2 complete: selected player move on mouse position

--- Assets/Week 7/Scripts/Controller.cs
+++ Assets/Week 7/Scripts/Controller.cs
@@ -3,9 +3,15 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Experimental.GraphView;
 
 public class Controller : MonoBehaviour
 {
+    float charge;
+    Vector2 direction;
+    public float maxCharge = 1;
+    public Slider chargeSlider;
+
     public static FootballPlayer SelectedPlayer { get; private set; }
 
     public static void SetSelectedPlayer(FootballPlayer player)
@@ -17,5 +23,35 @@ public class Controller : MonoBehaviour
         player.Selected(true);
         SelectedPlayer = player;
     }
-    
+
+    private void FixedUpdate()
+    {
+        if (direction != Vector2.zero)
+        {
+            SelectedPlayer.Move(direction);
+            direction = Vector2.zero;
+            charge = 0;
+            chargeSlider.value = charge;
+        }
+    }
+    private void Update()
+    {
+        if (SelectedPlayer == null) return;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            charge = 0;
+            direction = Vector2.zero;
+        }
+        if(Input.GetKey(KeyCode.Space))
+        {
+            charge += Time.deltaTime;
+            Mathf.Clamp(charge, 0, maxCharge);
+            chargeSlider.value = charge;
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - SelectedPlayer.transform.position).normalized * charge;
+        }
+    }
 }
--- Assets/Week 7/Scripts/FootballPlayer.cs
+++ Assets/Week 7/Scripts/FootballPlayer.cs
@@ -3,18 +3,22 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class FootballPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
 
     SpriteRenderer sr;
+    Rigidbody2D rb;
     public Color selectedColor;
     public Color unselectedColor;
+    public float speed = 10f;
 
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
         Selected(false);
     }
 
@@ -32,5 +36,10 @@ public class FootballPlayer : MonoBehaviour
             sr.color = unselectedColor;
         }
         
+    }
+
+    public void Move(Vector2 direction)
+    {
+        rb.AddForce(direction * speed, ForceMode2D.Impulse);
     }
 }
