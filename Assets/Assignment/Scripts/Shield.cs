6538b7f81f8355d1ac5249f7ce16a83ff4c7c583
charlesnyana <nyanasen@sheridancollege.ca>
2024-02-23 14:51:17 -0500

    A2: Shield moves on left mouse click (#4a item complete).

--- Assets/Assignment/Scripts/Shield.cs
+++ Assets/Assignment/Scripts/Shield.cs
@@ -3,18 +3,41 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Security.Cryptography.X509Certificates;
 
 public class Shield : MonoBehaviour
 {
+    Vector2 movement;
+    Vector2 destination;
+    public float speed = 1f;
+
+    Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        movement = destination - (Vector2)transform.position; // A2 Item #4a
+
+        if (movement.magnitude < 0.1)
+        {
+            movement = Vector2.zero;
+        }
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // gets coords for destination of shield on left click.
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(destination);
+        }
     }
 }
