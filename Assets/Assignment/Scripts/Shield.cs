9559528c238f50c82257eec9f1866772675535b6
charlesnyana <nyanasen@sheridancollege.ca>
2024-02-23 16:06:31 -0500

    A2 Shield: shield turns 90 degrees on z rotation on right click, and returns back to 0 on another right click.

--- Assets/Assignment/Scripts/Shield.cs
+++ Assets/Assignment/Scripts/Shield.cs
@@ -10,17 +10,24 @@ public class Shield : MonoBehaviour
     Vector2 movement;
     Vector2 destination;
     public float speed = 1f;
+    float currentDegree = 0;
+    float maxDegree = 90f;
+    public float rSpeed = 1f;
 
     Rigidbody2D rb;
+
+    bool isHorizontal;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isHorizontal = true;
     }
 
     private void FixedUpdate()
     {
+        // movement logic on left click
         movement = destination - (Vector2)transform.position; // A2 Item #4a
 
         if (movement.magnitude < 0.1)
@@ -28,6 +35,19 @@ public class Shield : MonoBehaviour
             movement = Vector2.zero;
         }
         rb.MovePosition(rb.position + movement.normalized * speed * Time.deltaTime);
+
+        // shield rotate logic
+        
+        {
+            // rotates 90 degrees
+            if (isHorizontal)
+            {
+                rb.rotation = currentDegree;
+            } else
+            {
+                rb.rotation = maxDegree;
+            }
+        }
     }
 
     // Update is called once per frame
@@ -38,6 +58,12 @@ public class Shield : MonoBehaviour
         {
             destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Debug.Log(destination);
+        }
+
+        if (Input.GetMouseButtonDown(1) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            isHorizontal = !isHorizontal;
+            Debug.Log("Is shield horizontal? " + isHorizontal);
         }
     }
 }
