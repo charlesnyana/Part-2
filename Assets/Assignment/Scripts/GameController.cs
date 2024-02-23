a360ef7afa013364dc4647b280d300542b1bdaec
charlesnyana <nyanasen@sheridancollege.ca>
2024-02-23 18:22:15 -0500

    A2 Attunement system: shield and gem colors change on spacebar input. established the attuneIndex variable which will be important for color matching checks later.

--- Assets/Assignment/Scripts/GameController.cs
+++ Assets/Assignment/Scripts/GameController.cs
@@ -7,17 +7,41 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject attuneUI;
+    public Image attuneUI;
+    public List <Sprite> attuneGem = new List<Sprite>();
+    public List <Color> attuneColor = new List<Color>();
+    int attuneIndex;
+
+
     public GameObject shield;
+    SpriteRenderer shieldSR;
+
+    private void Start()
+    {
+        attuneIndex = 0;
+        shieldSR = shield.GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        // Attunement system trigger. Will cycle between attunement types on spacebar.
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            attuneIndex = (attuneIndex + 1) % 3; // caps at 3
+            Debug.Log("Attunment index: " +attuneIndex);
+            attune(attuneIndex);
+        }
+    }
 
     public void takeDmg()
     {
 
     }
 
-    void attuneBlue()
+    void attune(int attunetype)
     {
-        
+        attuneUI.sprite = attuneGem[attunetype]; // 0 is blue, 1 is red, 2 is yellow.
+        shieldSR.color = attuneColor[attunetype];
     }
 
 }
